e45722fe16c8304608b88e33dfbe27f3f2dcfc8a
Shiroiii <dima.akarawak@gmail.com>
2023-01-11 01:40:35 +0100

    made some changes and added tests for the classifier

--- classification/KNNClassifier.py
+++ classification/KNNClassifier.py
@@ -3,15 +3,26 @@ import numpy as np
 import pandas as pd
 from scipy.spatial.distance import euclidean
 
-class KNN:
+class KNNClassifier:
     def __init__(self) -> None:
         self.X = None,
         self.y = None,
         self._num_of_classes = None
         self.k = 3
 
-    def fit(self, X:Union[np.ndarray, pd.DataFrame], y:Union[np.ndarray, pd.Series]) -> None:
-        """Stores the X and y values as numpy arrays to be used for predictions."""
+
+    def fit(self, X:Union[np.ndarray, pd.DataFrame], y:Union[np.ndarray, pd.Series]) -> None :
+        """
+        Stores the X and y values as numpy arrays to be used for predictions.
+        Parameters
+        ----------
+        X: data matrix of features
+        y: class labels as integers
+
+        Returns
+        -------
+        None
+        """
         if isinstance(X, pd.DataFrame) and isinstance(y, pd.Series):
             self.X = X.values
             self.y = y.values
@@ -21,11 +32,34 @@ class KNN:
             self.y = y
             self._num_of_classes = len(np.unique(self.y))
 
+
     def indicator(self, j:int, y_i:int) -> bool:
+        """
+        This indicator function takes to inputs are returns true if they're the same and false if they not
+
+        Parameters
+        ----------
+        j: An integer denoting the current class being compared
+        y_i: An integer denoting the class of observation i
+
+        Returns
+        -------
+        boolean
+        """
         return j == y_i
 
-    def _generate_n_neighbors(self, x:np.ndarray) -> np.array:
-        # print(x)
+
+    def _generate_k_neighbors(self, x:np.ndarray) -> np.array:
+        """
+        Generates an array of k closest neighbors to x in terms of the euclidean distance between them
+        Parameters
+        ----------
+        x:np.array = the array of features to be classified
+
+        Returns
+        -------
+        neighbors:np.array
+        """
         if self.X.shape[1] != x.shape[1]:
             raise ValueError("input x must have the same number of columns as the data matrix")
 
@@ -37,7 +71,10 @@ class KNN:
                 distance.append(euclidean(X[i, :], x))
 
         idx_array = np.argpartition(distance, self.k)
-        return self.y[idx_array][:self.k].T
+        neighbors = self.y[idx_array][:self.k].T
+
+        return neighbors
+
 
     def _generate_probs(self, y:np.ndarray) -> np.array:
         probs = np.zeros((self._num_of_classes, 1))
@@ -46,10 +83,11 @@ class KNN:
 
         return probs
 
+
     def predict(self, x:np.ndarray, k:int=None) -> int:
         if k is not None:
             self.k = k
-        neighbors =  self._generate_n_neighbors(x)
+        neighbors =  self._generate_k_neighbors(x)
         class_probabilities = self._generate_probs(neighbors)
         return np.argmax(class_probabilities)
 
@@ -58,6 +96,6 @@ if __name__ == '__main__':
     X = np.array([[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]]).T
     y = np.array([[0, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0]]).T
 
-    knn = KNN()
+    knn = KNNClassifier()
     knn.fit(X, y)
     print(knn.predict(np.array([[3]]), k=7))
