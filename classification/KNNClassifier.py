4ffedd92400f2d2fcb9041b6410415531cc9f0cb
Shiroiii <dima.akarawak@gmail.com>
2023-01-11 01:29:08 +0100

    added a requirements textfile and changed the classifier name

--- classification/KNNClassifier.py
+++ classification/KNNClassifier.py
@@ -3,10 +3,10 @@ import numpy as np
 import pandas as pd
 from scipy.spatial.distance import euclidean
 
-class KNN:
+class KNNClassifier:
     def __init__(self) -> None:
-        self.X = None,
-        self.y = None,
+        self.X = None
+        self.y = None
         self._num_of_classes = None
         self.k = 3
 
@@ -29,12 +29,12 @@ class KNN:
         if self.X.shape[1] != x.shape[1]:
             raise ValueError("input x must have the same number of columns as the data matrix")
 
-        if X.shape[1] < 2:
+        if self.X.shape[1] < 2:
             distance = [euclidean(x_i, x[0]) for x_i in self.X]
         else:
             distance = []
             for i in range(self.X.shape[0]):
-                distance.append(euclidean(X[i, :], x))
+                distance.append(euclidean(self.X[i, :], x))
 
         idx_array = np.argpartition(distance, self.k)
         return self.y[idx_array][:self.k].T
@@ -46,8 +46,9 @@ class KNN:
 
         return probs
 
-    # TODO: Implement the predict function to return the class for which it thinks is right
     def predict(self, x:np.ndarray, k:int=None) -> int:
+        if self.X is None or self.y is None:
+            raise TypeError("Please fit the model before trying to use it for predictions")
         if k is not None:
             self.k = k
         neighbors =  self._generate_n_neighbors(x)
@@ -60,6 +61,6 @@ if __name__ == '__main__':
     X = np.array([[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]]).T
     y = np.array([[0, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0]]).T
 
-    knn = KNN()
-    knn.fit(X, y)
+    knn = KNNClassifier()
+    # knn.fit(X, y)
     print(knn.predict(np.array([[3]]), k=7))
